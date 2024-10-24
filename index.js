3212b3bd6cd3bb2407f66ae77292c49195468eaf
Abunuman <abunumanpharm@gmail.com>
2024-10-25 00:59:03 +0100

    Changes on Portfolio

---
+++ index.js
@@ -0,0 +1,9 @@
+// Fetch Request
+const counter = document.querySelector(".counter-number");
+async function updateCounter() {
+    let response = await fetch("https://t37lson4piklgfks7vzl53jckq0ocpee.lambda-url.us-east-1.on.aws/ ");
+    let data = await response.json();
+    counter.innerHTML = `Views: ${data}`;
+}
+
+updateCounter();
